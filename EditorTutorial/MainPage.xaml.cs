46da6907266c4f88f5cce39b3b805fea95752b06
Pivnev Vladislav <steneox@gmail.com>
2021-11-18 19:35:50 +0300

    Добавьте файлы проекта.

---
+++ EditorTutorial/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EditorTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+
+        void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEditorCompleted(object sender, EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
